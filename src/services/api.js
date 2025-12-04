436cbad2bf4d832319845f887488c4da62b90804
kelvyn2012 <kudufo@gmail.com>
2025-12-04 13:03:07 +0100

    replaced my base url with that of render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://airbnb-no7gc9zsh-kelvyn2012s-projects.vercel.app',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8001',
   headers: {
     'Content-Type': 'application/json',
   },
